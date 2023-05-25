7e680fb932c237110a8bcd588fe06da7057f1aaf
KyoungJun <rudwns4188@gmail.com>
2023-05-25 21:43:04 +0900

    String

--- ch02/src/ch02/VarEx3.java
+++ ch02/src/ch02/VarEx3.java
@@ -15,10 +15,17 @@ public class VarEx3 {
 		float f = 3.14f;
 		double d = 3.14f;
 		
-		System.out.println(10.);
-		System.out.println(.10);
-		System.out.println(l);
-		System.out.println(10f);
-		System.out.println(1e3);
+		char ch = 'A';
+		int i = 'A';
+		
+		String str = "";
+		String str2 = "ABCD";
+		String str3 = "123";
+		String str4 = str2 + str3;
+		
+		System.out.println(ch);
+		System.out.println(i);
+		System.out.println(str4);
+		System.out.println(7+7+"");
 	}
 }
